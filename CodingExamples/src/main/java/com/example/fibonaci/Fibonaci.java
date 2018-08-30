4878d67439377cb57a1b5e3aea2a0c11f96a07be
Elison Nayak <nayakelisoon@gmail.com>
2018-08-29 22:51:33 -0400

    Initial CheckIn

---
+++ CodingExamples/src/main/java/com/example/fibonaci/Fibonaci.java
@@ -0,0 +1,38 @@
+package com.example.fibonaci;
+
+import java.util.Scanner;
+
+public class Fibonaci {
+	
+	public static void main(String[] args) {
+		System.out.println("Enter number upto which Fibonacci series to print: ");
+		int number = new Scanner(System.in).nextInt();
+		
+		System.out.println("Fibonacci series upto " + number +" numbers : ");
+		for(int i=1;i<=number;i++) {
+			System.out.println(fibonacciItetation(i));
+		}
+	}
+
+	private static int fibonacciRecursion(int i) {
+		if(i == 1 || i == 2) {
+			return 1;
+		}
+		return fibonacciRecursion(i-1)+fibonacciRecursion(i-2);
+	}
+	
+	public static int fibonacciItetation(int number){ 
+		if(number == 1 || number == 2){ 
+			return 1; 
+		} 
+		int fibo1=1, fibo2=1, fibonacci=1;
+		
+		for(int i=3;i<=number;i++){ 
+			//Fibonacci number is sum of previous two Fibonacci number 
+			fibonacci = fibo1 + fibo2; 
+			fibo1 = fibo2; 
+			fibo2 = fibonacci; 
+		} 
+		return fibonacci; //Fibonacci number 
+	}
+}
---
+++ CodingExamples/src/main/java/com/example/prime/PrimeCheck.java
@@ -0,0 +1,44 @@
+package com.example.prime;
+
+import java.util.Scanner;
+
+public class PrimeCheck {
+	public static void main(String[] args) {
+		Scanner scnr = new Scanner(System.in); 
+		int number = Integer.MAX_VALUE; 
+		System.out.println("Enter number to check if prime or not "); 
+		while (number != 0) {
+				number = scnr.nextInt(); 
+				System.out.printf("Does %d is prime? %s", 
+						number, 
+						isPrime(number));
+						//isPrimeOrNot(number), 
+						//isPrimeNumber(number)); 
+			}
+
+	}
+
+	private static boolean isPrime(int number) {
+		// TODO Auto-generated method stub
+		int sqrt = (int)Math.sqrt(number) + 1;
+		System.out.println("SQRT"+sqrt);
+		for(int i = 2;i<sqrt;i++) {
+			if(number % i == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	private static Object isPrimeOrNot(int number) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private static Object isPrimeNumber(int number) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+
+}
